235a1c1d5ed4d1ab1cd74b9ef3395c91aba61300
ysaury06 <ysaury.britomejia@student.uncommonhs.org>
2016-01-12 22:11:21 +0000

    Basketball project

---
+++ basketball.js
@@ -0,0 +1,24 @@
+function promptForPlayerName(player){       
+   var greeting = player +  ", what is your name?"
+   var name=prompt (greeting);
+   if (name.length>15) {
+    alert ("Wow, that’s a long name!");
+   }
+   
+   return name;
+    
+}
+function tryTwoPointShot() { 
+    var x= Math.random  ();
+    var x=Math.random  ();
+    if(x>0.2)
+    { return true;}
+    
+}
+function  tryThreePointShot(){ 
+   
+}
+
+function getShotString(){}
+
+function  isEndOfGame(){}
